b2b1791cd729979a3781619d3a9cc98e4a55628b
Yuki hayashi <43997348+Uki884@users.noreply.github.com>
2020-04-30 17:29:56 +0900

    Update db_spec.rb

--- spec/db/db_spec.rb
+++ spec/db/db_spec.rb
@@ -1,5 +1,5 @@
 describe "MySQLにrootユーザでログインできる" do
-    describe command('mysql -h mf-kts-db.crqlrnx9g0bw.ap-northeast-1.rds.amazonaws.com -P 3306 -u root -pkts_demo -e "SELECT user FROM mysql.user;"') do
+    describe command('mysql -h ******.ap-northeast-1.rds.amazonaws.com -P 3306 -u root -p*** -e "SELECT user FROM mysql.user;"') do
         its(:exit_status) { should eq 0 }
     end
 end
